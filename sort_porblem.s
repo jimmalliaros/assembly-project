70fb42eb3aabc898134d6e0c76cf447dd970741d
Jimmaliaros <jimmaliaros@gmail.com>
2025-01-01 17:05:37 +0200

    added comments

--- sort_porblem.s
+++ sort_porblem.s
@@ -1,31 +1,29 @@
+.data
+	str1:	.asciiz "Array["
+	str2:	.asciiz "]"
+.text
 
-
-sortArray:
-	add	$sp,$sp,4
-	sw	$ra,($sp)
+	li	$t0,1
 	
-	
-	la	$a0,sort_str
+	la	$a0,str1
 	li	$v0,4
 	syscall
 	
-	lw	$t0,N
-	sub	$t0,t0,1
-	la	$t1,array
-	li	$t2,0#i
-	li	$t3,0#j
+	move	$a0,$t0
+	li		$v0,1
+	syscall
 	
-	first_loop:
-		bge	$t2,$t0,done
-			sub	$t4,$t0,$t2
-			second_loop:
-					bge	$t3,$t4,first_loop
-					la	$a1,array
-					move	$a2,$t3
-					add	$t2,$t2,1
-					add	$t3,$t3,1
-					jal		swap
-					j	second_loop
+	la		$a0,str2
+	li		$v0,4
+	syscall
+	
+	li		$v0,10
+	syscall
+	
+	
+	
+
+
 					
 			
 					
